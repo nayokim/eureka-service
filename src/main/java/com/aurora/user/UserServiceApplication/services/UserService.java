1cdf0dda63e7a937e51b2ae97365e1587c999a90
nayokim <nayoung.kimpak@gmail.com>
2021-07-21 11:20:21 -0500

    service with business logic

--- src/main/java/com/aurora/user/UserServiceApplication/services/UserService.java
+++ src/main/java/com/aurora/user/UserServiceApplication/services/UserService.java
@@ -7,6 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Service: handles business logic
@@ -17,6 +21,11 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
+    /**
+     * save the user and return the saved user details
+     * @param inputUser
+     * @return
+     */
     @Transactional
     public UserDetailsDTO saveUpdate(UserDetailsDTO inputUser){
         try{
@@ -28,5 +37,42 @@ public class UserService {
             LOGGER.warn("exception in userService -> saveUpdate()" + e);
         }
         return null;
+    }
+
+    /**
+     * get user details based on id
+     * @param id
+     * @return
+     */
+    public UserDetailsDTO getById(Long id){
+        return getUserDetailsDTO(repository.getOne(id));
+    }
+
+    /**
+     * Returns the user detials list based on name
+     * @param name
+     * @return
+     */
+
+    public List<UserDetailsDTO> getByName(String name){
+        List<UserDetails> userDetailsList = repository.findUserByName(name);
+        if(CollectionUtils.isEmpty(userDetailsList)){
+            return null;
+        }
+        return userDetailsList.stream().map(this::getUserDetailsDTO).collect(Collectors.toList());
+    }
+
+    /**
+     *
+     * @param userDetails
+     * @return
+     */
+
+    public UserDetailsDTO getUserDetailsDTO(UserDetails userDetails){
+        return new UserDetailsDTO(
+                userDetails.getId().toString(),
+                userDetails.getName(),
+                userDetails.getAge().toString()
+        );
     }
 }
