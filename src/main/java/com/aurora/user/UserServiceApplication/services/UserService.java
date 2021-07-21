d5036adb811702615902890e60ce12f8c80eb6d4
nayokim <nayoung.kimpak@gmail.com>
2021-07-21 10:01:32 -0500

    start service

--- src/main/java/com/aurora/user/UserServiceApplication/services/UserService.java
+++ src/main/java/com/aurora/user/UserServiceApplication/services/UserService.java
@@ -1,4 +1,26 @@
 package com.aurora.user.UserServiceApplication.services;
 
+import com.aurora.user.UserServiceApplication.dtos.UserDetailsDTO;
+import com.aurora.user.UserServiceApplication.entities.UserDetails;
+import com.aurora.user.UserServiceApplication.repositories.UserRepository;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Service: handles business logic
+ */
 public class UserService {
+    private final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserRepository repository;
+
+    @Transactional
+    public UserDetailsDTO saveUpdate(UserDetailsDTO inputUser){
+        try{
+            UserDetails userDetails = new UserDetails();
+
+        }
+    }
 }
