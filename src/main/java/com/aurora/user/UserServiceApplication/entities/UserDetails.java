dfc23a5ce2364a34432c7e4473d46c9b94624379
nayokim <nayoung.kimpak@gmail.com>
2021-07-21 10:22:41 -0500

    set up entity

--- src/main/java/com/aurora/user/UserServiceApplication/entities/UserDetails.java
+++ src/main/java/com/aurora/user/UserServiceApplication/entities/UserDetails.java
@@ -1,4 +1,49 @@
 package com.aurora.user.UserServiceApplication.entities;
 
+import sun.util.resources.Bundles;
+
+import javax.persistence.*;
+
+//entities: represent the database table as objects
+@Entity
+@Table
 public class UserDetails {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="age")
+    private Integer age;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id=id;
+    }
+
+    public String getName(String name){
+        return this.name;
+    }
+
+    public void setName(){
+        this.name=name;
+    }
+
+    public Integer getAge(){
+        return this.age;
+    }
+
+    public void setAge(Integer age){
+        this.age=age;
+    }
+
+
+
+
+
 }
