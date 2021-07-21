e1ab6a0237e82ef75eb144455a0092a860f5558b
nayokim <nayoung.kimpak@gmail.com>
2021-07-21 11:07:00 -0500

    not done

--- src/main/java/com/aurora/user/UserServiceApplication/services/UserService.java
+++ src/main/java/com/aurora/user/UserServiceApplication/services/UserService.java
@@ -3,6 +3,7 @@ package com.aurora.user.UserServiceApplication.services;
 import com.aurora.user.UserServiceApplication.dtos.UserDetailsDTO;
 import com.aurora.user.UserServiceApplication.entities.UserDetails;
 import com.aurora.user.UserServiceApplication.repositories.UserRepository;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,7 +21,12 @@ public class UserService {
     public UserDetailsDTO saveUpdate(UserDetailsDTO inputUser){
         try{
             UserDetails userDetails = new UserDetails();
-
+            userDetails.setName(inputUser.getName());
+            userDetails.setAge(Integer.parseInt(inputUser.getAge()));
+            return getUserDetailsDTO(repository.save(userDetails));
+        }catch (Exception e){
+            LOGGER.warn("exception in userService -> saveUpdate()" + e);
         }
+        return null;
     }
 }
